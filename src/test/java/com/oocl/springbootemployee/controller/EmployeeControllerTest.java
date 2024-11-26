cd0cf5cf050533cd76b90629312cbae8e88908e8
WONGTO6 <wongto6@oocl.com>
2024-11-26 17:15:45 +0800

    test: finish should_throw_EmployeeInactiveException_when_update_inactive_employee

--- src/test/java/com/oocl/springbootemployee/controller/EmployeeControllerTest.java
+++ src/test/java/com/oocl/springbootemployee/controller/EmployeeControllerTest.java
@@ -160,7 +160,7 @@ class EmployeeControllerTest {
     @Test
     void should_update_employee_success() throws Exception {
         // Given
-        Integer givenId = 1;
+        Integer givenId = employeeRepository.findAll().get(0).getId();
         String givenName = "New Employee";
         Integer givenAge = 30;
         Gender givenGender = Gender.FEMALE;
@@ -197,7 +197,7 @@ class EmployeeControllerTest {
     @Test
     void should_remove_employee_success() throws Exception {
         // Given
-        int givenId = 1;
+        int givenId = employeeRepository.findAll().get(0).getId();
 
         // When
         // Then
@@ -205,10 +205,10 @@ class EmployeeControllerTest {
             .andExpect(MockMvcResultMatchers.status().isNoContent());
         List<Employee> employees = employeeRepository.findAll();
         assertThat(employees).hasSize(4);
-        assertThat(employees.get(0).getId()).isEqualTo(2);
-        assertThat(employees.get(1).getId()).isEqualTo(3);
-        assertThat(employees.get(2).getId()).isEqualTo(4);
-        assertThat(employees.get(3).getId()).isEqualTo(5);
+        assertThat(employees.get(0).getId()).isEqualTo(employeeRepository.findAll().get(0).getId());
+        assertThat(employees.get(1).getId()).isEqualTo(employeeRepository.findAll().get(1).getId());
+        assertThat(employees.get(2).getId()).isEqualTo(employeeRepository.findAll().get(2).getId());
+        assertThat(employees.get(3).getId()).isEqualTo(employeeRepository.findAll().get(3).getId());
     }
 
     @Test
