32ea7040d1fa9c1d4b62cda14b32ef338ddf6522
WONGTO6 <wongto6@oocl.com>
2024-11-26 17:57:36 +0800

    feat: change to compare id

--- src/test/java/com/oocl/springbootemployee/controller/EmployeeControllerTest.java
+++ src/test/java/com/oocl/springbootemployee/controller/EmployeeControllerTest.java
@@ -151,6 +151,7 @@ class EmployeeControllerTest {
             .andExpect(MockMvcResultMatchers.jsonPath("$.salary").value(givenSalary));
         List<Employee> employees = employeeRepository.findAll();
         assertThat(employees).hasSize(1);
+        assertThat(employees.get(0).getId()).isEqualTo(employeeRepository.findAll().get((int) employeeRepository.count() -1 ).getId());
         assertThat(employees.get(0).getName()).isEqualTo(givenName);
         assertThat(employees.get(0).getAge()).isEqualTo(givenAge);
         assertThat(employees.get(0).getGender()).isEqualTo(givenGender);
@@ -188,6 +189,7 @@ class EmployeeControllerTest {
             .andExpect(MockMvcResultMatchers.jsonPath("$.salary").value(givenSalary));
         List<Employee> employees = employeeRepository.findAll();
         assertThat(employees).hasSize(5);
+        assertThat(employees.get(0).getId()).isEqualTo(employeeRepository.findById(givenId).orElseThrow().getId());
         assertThat(employees.get(0).getName()).isEqualTo(givenName);
         assertThat(employees.get(0).getAge()).isEqualTo(givenAge);
         assertThat(employees.get(0).getGender()).isEqualTo(givenGender);
